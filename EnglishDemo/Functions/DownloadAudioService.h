24b9c36fb013ec83eb5771c43b5961fcd19b316c
mayixuan <1823341995@qq.com>
2019-07-07 21:53:43 +0800

    增加音频缓存

---
+++ EnglishDemo/Functions/DownloadAudioService.h
@@ -0,0 +1,17 @@
+//
+//  DownloadAudioService.h
+//  EnglishDemo
+//
+//  Created by 马一轩 on 2019/7/7.
+//  Copyright © 2019 马一轩. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DownloadAudioService : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ EnglishDemo/Functions/DownloadAudioService.m
@@ -0,0 +1,13 @@
+//
+//  DownloadAudioService.m
+//  EnglishDemo
+//
+//  Created by 马一轩 on 2019/7/7.
+//  Copyright © 2019 马一轩. All rights reserved.
+//
+
+#import "DownloadAudioService.h"
+
+@implementation DownloadAudioService
+
+@end
